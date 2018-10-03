f9ffcb92db95acd0320ee9a24ce88194fa82a5ec
Emma Orhun <emma.orhun@shopify.com>
2018-10-03 15:45:22 -0400

    Initial commit

---
+++ myt03/Array.h
@@ -0,0 +1,19 @@
+#define MAX_ARR_SIZE  128
+#include "Book.h"
+
+#ifndef ARRAY_H
+#define ARRAY_H
+
+class Array
+{
+  public:
+    Array();
+    void Add(Book&);
+    void print();
+
+  private:
+    int size;
+    Book elements[MAX_ARR_SIZE];
+};
+
+#endif
